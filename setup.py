1381f98f850041bd26c7013cde9a2a222613d137
Deyner lopez <dlopez@allcode.com>
2026-08-18 14:57:20 -0500

    feat: added a new integration test for jenkins pipeline

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ccai',
-    version='1.1.0',
+    version='1.2.0',
     description='A CLI and Python client for sending SMS via CloudContactAI',
     author='CloudContactAI LLC',
     license='MIT',
